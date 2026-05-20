180630408220365771ec7e3d7679ef1b494f30b0
zeet <3105327090@qq.com>
2026-05-20 17:26:58 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::panic;
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -10,20 +10,39 @@ fn main() {
         io::stdin().read_line(&mut input).unwrap();
         let args: Vec<&str> = input.split_whitespace().collect();
         if !args.is_empty() {
-            let cmd = args[0];
-            let cmd_args = &args[1..];
-            match cmd {
-                "exit" => {
-                    break;
-                }
-                "echo" => {
-                    let output = cmd_args.join(" ");
-                    println!("{}", output);
-                }
-                _ => {
-                    println!("{}: command not found", cmd);
-                }
-            }
+            execute(&args);
         }
+    }
+}
+
+fn is_buildin(cmd: &str) -> bool {
+    match cmd {
+        "exit" | "echo" | "type" => true,
+        _ => false,
+    }
+}
+
+fn execute(args: &[&str]) {
+    if is_buildin(args[0]) {
+        execute_builtin(args);
+    } else {
+        println!("{}: command not found", args[0]);
+    }
+}
+
+fn execute_builtin(args: &[&str]) {
+    match args[0] {
+        "exit" => std::process::exit(0),
+        "echo" => println!("{}", args[1..].join(" ")),
+        "type" => {
+            args[1..].iter().for_each(|s| {
+                if is_buildin(s) {
+                    println!("{} is a shell builtin", s);
+                } else {
+                    println!("{}: not found", s);
+                }
+            });
+        }
+        _ => panic!(),
     }
 }
