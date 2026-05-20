4bc57585601c602497ab3cd2ce8990a9eae5e767
zeet <3105327090@qq.com>
2026-05-20 19:13:10 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,76 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
-use std::{os::unix::fs::MetadataExt, panic};
+use std::{os::unix::fs::MetadataExt, panic, process::Command};
 
 fn main() {
-    loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
-        let args: Vec<&str> = input.split_whitespace().collect();
-        if !args.is_empty() {
-            execute(&args);
-        }
+  loop {
+    print!("$ ");
+    io::stdout().flush().unwrap();
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let args: Vec<&str> = input.split_whitespace().collect();
+    if !args.is_empty() {
+      execute(&args);
     }
+  }
 }
 
 fn is_buildin(cmd: &str) -> bool {
-    match cmd {
-        "exit" | "echo" | "type" => true,
-        _ => false,
-    }
+  match cmd {
+    "exit" | "echo" | "type" => true,
+    _ => false,
+  }
 }
 
 fn execute(args: &[&str]) {
-    if is_buildin(args[0]) {
-        execute_builtin(args);
-    } else {
-        println!("{}: command not found", args[0]);
+  if is_buildin(args[0]) {
+    execute_builtin(args);
+  } else {
+    let cmd = args[0];
+    match find_executable(cmd) {
+      Some(p) => {
+        let _ = Command::new(p).args(&args[1..]).status();
+      }
+      None => println!("{}: command not found", cmd),
     }
+  }
 }
 
 fn execute_builtin(args: &[&str]) {
-    match args[0] {
-        "exit" => std::process::exit(0),
-        "echo" => println!("{}", args[1..].join(" ")),
-        "type" => {
-            args[1..].iter().for_each(|s| {
-                if is_buildin(s) {
-                    println!("{} is a shell builtin", s);
-                } else {
-                    match find_executable(s) {
-                        Some(p) => println!("{} is {}", s, p.display()),
-                        None => println!("{}: not found", s),
-                    }
-                }
-            });
+  match args[0] {
+    "exit" => std::process::exit(0),
+    "echo" => println!("{}", args[1..].join(" ")),
+    "type" => {
+      args[1..].iter().for_each(|s| {
+        if is_buildin(s) {
+          println!("{} is a shell builtin", s);
+        } else {
+          match find_executable(s) {
+            Some(p) => println!("{} is {}", s, p.display()),
+            None => println!("{}: not found", s),
+          }
         }
-        _ => panic!(),
+      });
     }
+    _ => panic!(),
+  }
 }
 
 fn find_executable(cmd: &str) -> Option<std::path::PathBuf> {
-    let path_os = std::env::var_os("PATH")?;
-    for dir in std::env::split_paths(&path_os) {
-        let full_path = dir.join(cmd);
-        if is_executable(&full_path) {
-            return Some(full_path);
-        }
+  let path_os = std::env::var_os("PATH")?;
+  for dir in std::env::split_paths(&path_os) {
+    let full_path = dir.join(cmd);
+    if is_executable(&full_path) {
+      return Some(full_path);
     }
-    None
+  }
+  None
 }
 
 fn is_executable(path: &std::path::Path) -> bool {
-    if let Ok(metadata) = path.metadata() {
-        metadata.is_file() && (metadata.mode() & 0o111) != 0
-    } else {
-        false
-    }
+  if let Ok(metadata) = path.metadata() {
+    metadata.is_file() && (metadata.mode() & 0o111) != 0
+  } else {
+    false
+  }
 }
