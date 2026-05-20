5050344a222b26903ebba617127b4d2c7c1708aa
zeet <3105327090@qq.com>
2026-05-20 14:27:16 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,6 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    // print!("$ ");
+    print!("$ ");
     // io::stdout().flush().unwrap();
 }
