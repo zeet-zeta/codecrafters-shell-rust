0516782793f8d52204116038021275646f7e0ab4
zeet <3105327090@qq.com>
2026-05-21 16:49:31 +0800

    codecrafters submit [skip ci]

--- src/parser.rs
+++ src/parser.rs
@@ -8,8 +8,17 @@ pub fn parse_command_line(input: &str) -> Vec<String> {
     let mut args = Vec::new();
     let mut current_arg = String::new();
     let mut state = State::Normal;
+    let mut escape_flag = false;
 
     for ch in input.chars() {
+        if ch == '\\' {
+            escape_flag = true;
+            continue;
+        }
+        if escape_flag {
+            current_arg.push(ch);
+            escape_flag = false;
+        }
         match state {
             State::Normal => match ch {
                 '\'' => {
